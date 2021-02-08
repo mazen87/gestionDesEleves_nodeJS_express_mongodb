aafcda4cfe1a8b8f87db94a0fc8d1f72a2cde4ac
mazen87 <mazen.karfoul87@hotmail.com>
2021-02-08 15:51:04 +0100

    dev

--- models/eleve.js
+++ models/eleve.js
@@ -1,15 +1,7 @@
-//const mongoose = require('mongoose');
+
 // définir notre elève model 
-// model.exports nous permet de transmettre ça vers autre fichier een lui appelant 
-/*
- module.exports = mongoose.model('Eleve',{
-     nom : {type : String},
-     prenom : {type : String},
-     dateDeNaissance : {type:Date},
-     moyen : {type:Number}   
- })
- */
-        //import mongoose from 'mongoose';
+
+
         const mongoose = require('mongoose');
         const Schema = mongoose.Schema;
 
